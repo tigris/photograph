d38fa4130e220d3acb7d331ca9d039ad03ee0576
Danial Pearce <git@tigris.id.au>
2013-12-23 23:02:33 +1100

    Spec cleanup

--- spec/photograph/artist_spec.rb
+++ spec/photograph/artist_spec.rb
@@ -2,72 +2,53 @@ require 'spec_helper'
 
 module Photograph
   describe Artist do
-    URL = "http://rubygems.org"
+    let(:url) { 'http://rubygems.org' }
 
-    it "should instanciate a new artist" do
-      Artist.new :url => URL
+    subject { Artist.new :url => url }
+
+    it { expect(subject).to be_kind_of(Artist) }
+
+    it('should accept a block when shooting') do
+      subject.shoot!{|image| image.should respond_to(:path) }
     end
 
-    it "should accept a block when shooting" do
-      Artist.new(:url => URL).shoot! do |image|
-        image.should respond_to(:path)
-      end
+    it('should raise an exception if no block was given when shooting') do
+      expect{ subject.shoot! }.to raise_error
     end
 
-    it "should raise an exception if not block was given when shooting" do
-      expect {Artist.new(:url => URL).shoot!}.to raise_error
-    end
-
-    it "should raise an error without an url" do
-      expect { Artist.new }.to raise_error(Artist::MissingUrlError)
+    it('should raise an error without an url') do
+      expect{ Artist.new }.to raise_error(Artist::MissingUrlError)
     end
 
     describe "Wrongly formed urls" do
-      it "should leave '#{URL} untouched as it is already valid" do
-        @artist = Artist.new :url => URL
-
-        expect(@artist.options[:url]).to eq(URL)
+      it('should leave http version of url untouched as it is already valid') do
+        expect(subject.options[:url]).to eq(url)
       end
 
-      it "should leave 'https://rubygems.org' untouched as it is already valid" do
-        @artist = Artist.new :url => 'https://rubygems.org'
-
-        expect(@artist.options[:url]).to eq('https://rubygems.org')
+      it('should leave https version of url untouched as it is already valid') do
+        expect(Artist.new(:url => 'https://rubygems.org').options[:url]).to eq('https://rubygems.org')
       end
 
-      it "should prepend 'http://' if protocol isn't present" do
-        @artist = Artist.new :url => "github.com"
-
-        expect(@artist.options[:url]).to eq("http://github.com")
+      it('should prepend http:// if protocol is not present') do
+        expect(Artist.new(:url => 'github.com').options[:url]).to eq('http://github.com')
       end
     end
 
-    describe "Default size values" do
-      before(:each) { @artist = Artist.new :url => URL }
+    describe 'Default size values' do
+      it('should have default value for x') { expect(subject.options[:x]).to eq(0) }
+      it('should have default value for y') { expect(subject.options[:y]).to eq(0) }
+      it('should have default value for w') { expect(subject.options[:w]).to eq(1280) }
+      it('should have default value for h') { expect(subject.options[:h]).to eq(1024) }
+    end
 
-      it "should have default values for x,y : 0,0" do
-        @artist.options[:x].should == 0
-        @artist.options[:y].should == 0
-      end
+    describe 'Cropping' do
+      subject { Artist.new :url => URL, :x => 200, :y => 100, :h => 400, :w => 400 }
+      before { Artist.browser.driver.stub(:render) }
 
-      it"should have default values for h,w : 1280, 1024" do
-        @artist.options[:w].should == 1280
-        @artist.options[:h].should == 1024
+      xit 'should take a screenshot large enough to crop later' do
+        subject.shoot!
       end
     end
 
-    describe "Cropping" do
-      before(:each) { @artist = Artist.new :url => URL }
-
-      it "should take a screenshot large enough to crop later" do
-        pending
-
-        @artist = Artist.new :url => URL, :x => 200, :y => 100, :h => 400, :w => 400
-
-        Artist.browser.driver.stub :render
-
-        @artist.shoot!
-      end
-    end
   end
 end
